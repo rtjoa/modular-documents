a7fb17288888ed9de4d6d2e7dc256b84f3401a4f
nareh <53708656+nareha@users.noreply.github.com>
2021-05-27 15:44:42 -0700

    Added a login button to the navbar and stylized the navbar

--- app/src/index.js
+++ app/src/index.js
@@ -20,13 +20,14 @@ class App extends React.Component {
             logged in users, and Welcome otherwise. For now, they are separate
             links.
           */}
-          <Link class="logo" to="/welcome">
+          <Link className="logo" to="/welcome">
             🧊🖊️
           </Link>
           <Link to="/">My Docs</Link>
           <Link to="/explore">Explore</Link>
           <Link to="/document/1">Edit Document 1</Link>
           <Link to="/view/1">View Document 1</Link>
+          <Link className="button" to="/">Login</Link>
         </nav>
 
         <Switch>
