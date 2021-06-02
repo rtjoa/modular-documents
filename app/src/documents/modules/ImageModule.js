71164b0a2b79ef8024d8944992b20c66fb98f92b
nareh <53708656+nareha@users.noreply.github.com>
2021-06-01 23:38:04 -0700

    Initial work on image rendering from module storage

--- app/src/documents/modules/ImageModule.js
+++ app/src/documents/modules/ImageModule.js
@@ -1,31 +1,77 @@
-import React from 'react';
-import ImageUploader from 'react-images-upload';
+import React, { useState } from 'react';
+import { storage } from '../../firebase.js';
+
 
 export function ImageModule(props) {
-  const onImageUpload = (image) => {
-    props.setEditing(false);
-    props.setData({ src: URL.createObjectURL(new Blob(image)) });
+  const [image, setImage] = useState(null);
+  const storageRef = storage.ref();
+
+  const onImageChange = (e) => {
+    const reader = new FileReader();
+    let file = e.target.files[0]; 
+    if (file) {
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          console.log(file);
+          setImage(file);
+          props.setData({ src: file.name })
+        }
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    } else {
+      setImage(null);
+    }
   };
+
+  async function uploadToFirebase(){
+    if (image) {
+      storageRef.child(image.name).put(image).then(() => {
+        alert("Image has been uploaded successfully!");
+      }).catch((error) => {
+        console.log(error)
+      });
+    } else {
+      alert("Please upload an image first.");
+    }
+  }
+
+  async function getFromFirebase(){
+    console.log("Request started")
+    if(image)
+      return await storageRef.child(image.name).getDownloadURL().then((url) => {
+        return url;
+      }).catch(() => {
+        console.log("Error retrieving image");
+      });
+    console.log("request failed")
+  }
+
+   /*const deleteFromFirebase = () => {
+    const storageRef = storage.ref();
+    const imageRef = storageRef.child(props.data.src);
+      imageRef.delete().then(() => {
+        console.log("Image deleted from database");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }; */
 
   if (props.editing) {
     return (
       <div className="image-upload-wrapper">
-        <ImageUploader
-          withIcon={true}
-          buttonText="Choose Image"
-          onChange={onImageUpload}
-          imgExtension={['.jpg', '.gif', '.png', '.gif']}
-          maxFileSize={5242880}
-          singleImage={true}
-          label=""
-        />
+        <input type="file" accept="image/x-png,image/jpeg" onChange={(e) => {onImageChange(e); }}/>
+        <button onClick={uploadToFirebase}>Upload Image</button>
+        <button onClick={() => getFromFirebase()}>
+            Get image for display
+        </button>
       </div>
     );
   } else {
     return (
       <div className="image-module">
-        <img src={props.data.src} />
-      </div>
+        <img src={getFromFirebase()}></img>
+       </div>
     );
   }
 }
