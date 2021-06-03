d109cf8d6237fe844ac5a522cf3a6473ad6dd311
rtjoa <51928404+rtjoa@users.noreply.github.com>
2021-06-03 05:36:02 -0700

    TextModule: Add align option

--- src/modules/TextModule.js
+++ src/modules/TextModule.js
@@ -6,18 +6,28 @@ import '../styles/TextModule.scss';
 
 export function TextModule(props) {
   const onTextChange = (event) => {
-      props.setData({ text: event.target.value });
+      props.setData({ ...props.data, text: event.target.value });
+  };
+
+  const onAlignChange = (event) => {
+      props.setData({ ...props.data, align: event.target.value });
   };
 
   if (props.editing) {
     return (
       <div className="text-module-edit">
         <textarea onChange={onTextChange} value={props.data.text}></textarea>
+        Align: <select onChange={onAlignChange} value={props.data.align}>
+          <option value="left">Left</option>
+          <option value="center">Center</option>
+          <option value="right">Right</option>
+          <option value="justify">Justify</option>
+        </select>
       </div>
     );
   } else {
     return (
-      <div className="text-module-view">
+      <div className={`text-module-view align-${props.data.align}`}>
         { props.data.text.trim() ? props.data.text.split('\n').map((line, i) => (
           <ReactMarkdown linkTarget="_blank" remarkPlugins={[gfm]} key={i}>{line}</ReactMarkdown>
         )) : <EmptyText/> }
@@ -32,6 +42,6 @@ function EmptyText(){
   )
 }
 
-TextModule.initData = { text: '' };
+TextModule.initData = { text: '', align: 'left' };
 TextModule.initTempData = null;
 TextModule.moduleName = 'Text';
