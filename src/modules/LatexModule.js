f10ade304191022fba0426c5e72345af7fbd2a77
rtjoa <51928404+rtjoa@users.noreply.github.com>
2021-06-03 04:28:12 -0700

    LatexModule: Add inline option, other tweaks
    - Add option to render LaTeX inline
    - Reduce margin of rendered LaTeX
    - Render errors with tooltips

--- src/modules/LatexModule.js
+++ src/modules/LatexModule.js
@@ -4,14 +4,36 @@ var Latex = require('react-latex');
 import '../styles/LatexModule.scss';
 
 export function LatexModule(props) {  
-    const onTextChange = (event) => {
-      props.setData({ text: event.target.value });
+  const onTextChange = (event) => {
+    props.setData({
+      ...props.data,
+      text: event.target.value
+    });
+  };
+
+  const onInlineCheckboxChange = (event) => {
+    props.setData({
+      ...props.data,
+      inline: event.target.checked
+    });
   };
 
   if (props.editing) {
+    const boxId = `latex-module-edit-${props.i}-box`;
     return (
       <div className="latex-module-edit">
-        <textarea onChange={onTextChange} value={props.data.text}></textarea>
+        <textarea
+          onChange={onTextChange}
+          value={props.data.text}
+          placeholder={"Type something like: $x^2 + y^2 = 1$"}
+        />
+        <input
+          id={boxId}
+          type="checkbox"
+          onChange={onInlineCheckboxChange}
+          checked={props.data.inline}
+        />
+        <label htmlFor={boxId}>Inline</label>
       </div>
     );
   } else {
@@ -19,7 +41,12 @@ export function LatexModule(props) {
       <div className="latex-module-view">
         { props.data.text.trim() ? props.data.text.split('\n').map((line, i) => (
         <div key={i}> 
-                <Latex displayMode={true}>{line}</Latex>
+          <Latex
+            displayMode={!props.data.inline}
+            throwOnError={false}
+          >
+              {line}
+          </Latex>
         </div>
         )) : <EmptyText/> }
       </div>
@@ -33,6 +60,6 @@ function EmptyText(){
   )
 }
 
-LatexModule.initData = { text: '' };
+LatexModule.initData = { text: '', inline: false };
 LatexModule.initTempData = null;
 LatexModule.moduleName = 'LaTeX';
